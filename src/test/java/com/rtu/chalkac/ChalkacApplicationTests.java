5c67e2198b0ee1bfb08834d47e01e30d1c7b742b
RyuTaeUung <aser77992@gmail.com>
2024-11-15 11:46:45 +0900

    feat:start Project

---
+++ src/test/java/com/rtu/chalkac/ChalkacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rtu.chalkac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChalkacApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
