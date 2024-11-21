0333b73ae6da8af1f6f0df9a6f213c522eb2196d
RyuTaeUung <aser77992@gmail.com>
2024-11-21 15:21:49 +0900

    deploy : deploy 테스트

--- src/main/java/com/rtu/chalkac/global/HealthCheckController.java
+++ src/main/java/com/rtu/chalkac/global/HealthCheckController.java
@@ -9,6 +9,6 @@ public class HealthCheckController {
 
     @GetMapping("/health")
     public ResponseEntity<String> healthCheck() {
-        return ResponseEntity.ok().body("OK!");
+        return ResponseEntity.ok().body("OK!!!!!!");
     }
 }
