47265455e723ffb30140c1122a23edba74b8b425
RyuTaeUung <aser77992@gmail.com>
2024-11-26 15:48:15 +0900

    fix : 경로 수정

--- src/main/java/com/rtu/chalkac/domain/reply/controller/ReplyController.java
+++ src/main/java/com/rtu/chalkac/domain/reply/controller/ReplyController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/replies")
+@RequestMapping("/api/v1/reply")
 @RequiredArgsConstructor
 public class ReplyController {
     private final ReplyService replyService;
